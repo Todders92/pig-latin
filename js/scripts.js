1922b9d14e22d03121b0d6633491da13fbe688a1
Student <example@example.com>
2019-12-18 20:33:10 -0800

    update logic

--- js/scripts.js
+++ js/scripts.js
@@ -11,13 +11,23 @@ $(document).ready(function() {
     var phrase = $("input#phrase").val();
     console.log(phrase);
 
+    var phraseArray = phrase.split("");
+    console.log(phraseArray);
+
     var result;
       if (isNaN(phrase) === false) {
         result = phrase;
-      } else if (phrase === "a" || "i" || "e"|| "u" || "o") {
+      } else if (["a", "e", "i", "o", "u"].includes(phraseArray[0])) {
         result = phrase + "ay";
-      } else if (phrase.includes("a" || "i" || "e"|| "u" || "o", 0)) {
-        result = phrase + "ay";
+        console.log(result);
+      } else if (["q", "w", "r", "t", "y", "p", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"].includes(phraseArray[0])) {
+        var clonePhraseArray = phraseArray.map(function(element) {
+          return element;
+        })
+        var removed = clonePhraseArray.splice(0, 1);
+        console.log(removed);
+        result = removed + phraseArray[0];
+        console.log(result);
       }
 
     $("#piglatinoutput").show();
